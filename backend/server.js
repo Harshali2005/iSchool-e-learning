89984060c56998d7101d5eeb3ea03d6a8f60c7d3
Harshali2005 <bagulharshu2005@gmail.com>
2025-11-21 12:33:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://e-learning-frontend.onrender.com'
-  ],
+  origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
